bb20dfc0319f91e277801e247c8c3da25d36fade
Marco Asbreuk <marco@itsasbreuk.nl>
2015-03-17 22:48:02 +0100

    added cart and printer icon

---
+++ base-icons/cart.js
@@ -0,0 +1,20 @@
+
+"use strict";
+/**
+ * Creating floating Panel-nodes which can be shown and hidden.
+ *
+ *
+ * <i>Copyright (c) 2014 ITSA - https://github.com/itsa</i>
+ * New BSD License - http://choosealicense.com/licenses/bsd-3-clause/
+ *
+ * SVG icon by https://icomoon.io/app/#/select
+ *
+ * @module panel
+ * @class Panel
+ * @since 0.0.1
+*/
+
+require('../css/alert.css');
+module.exports = function (window) {
+    window.document.defineIcon('cart', 1024, 1024, '<path class="path1" d="M384 928c0 53.019-42.981 96-96 96s-96-42.981-96-96c0-53.019 42.981-96 96-96s96 42.981 96 96z"></path><path class="path2" d="M1024 928c0 53.019-42.981 96-96 96s-96-42.981-96-96c0-53.019 42.981-96 96-96s96 42.981 96 96z"></path><path class="path3" d="M1024 512v-384h-768c0-35.346-28.654-64-64-64h-192v64h128l48.074 412.054c-29.294 23.458-48.074 59.5-48.074 99.946 0 70.696 57.308 128 128 128h768v-64h-768c-35.346 0-64-28.654-64-64 0-0.218 0.014-0.436 0.016-0.656l831.984-127.344z"></path>');
+};
